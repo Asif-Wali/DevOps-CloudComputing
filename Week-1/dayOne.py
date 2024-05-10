22eb801375f0baf25eb2c040184fa0cbadd25138
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-05-11 01:46:28 +0530

    first push of the new repo

---
+++ Week-1/dayOne.py
@@ -0,0 +1,10 @@
+# print('Hello World!')
+
+print("My name is Asif and I am learning Python for DevOps and Cloug Computing")
+
+def add (a, b):
+    return a + b
+
+print(add(1,2))
+name="Mohammad Asif"
+print(name[9])
